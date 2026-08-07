50175227bb966bac7c84c728c35d898a1429c00b
Graham Bunt <gtbunt@gmail.com>
2026-08-07 12:38:38 -0600

    Prevent final word orphans

--- src/lib/typography.ts
+++ src/lib/typography.ts
@@ -1,8 +1,12 @@
 const BREAKABLE_SPACE_PATTERN = /[ \t\r\n\f\v]+/g;
 const SINGLE_LETTER_WORD_PATTERN = /\b([AaI]) (?=\S)/g;
+const FINAL_WORD_PAIR_PATTERN = /(\S+) (\S+)$/;
 
 export function preventTextOrphans(text: string) {
-  return text.replace(BREAKABLE_SPACE_PATTERN, " ").replace(SINGLE_LETTER_WORD_PATTERN, "$1\u00a0");
+  return text
+    .replace(BREAKABLE_SPACE_PATTERN, " ")
+    .replace(SINGLE_LETTER_WORD_PATTERN, "$1\u00a0")
+    .replace(FINAL_WORD_PAIR_PATTERN, "$1\u00a0$2");
 }
 
 export function splitTypographicWords(text: string) {
